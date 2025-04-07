c1f7d954e5e702e39bc0a166debc0309a88f98e2
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2025-04-07 10:21:52 +0300

    chore: add unique key to FormattedMessage component

--- src/components/FormattedMessage/FormattedMessage.tsx
+++ src/components/FormattedMessage/FormattedMessage.tsx
@@ -1,5 +1,7 @@
-import type { IntlMessageID } from 'i18n/messages';
+import { v4 as uuid } from 'uuid';
 import { FormattedMessage as ReactFormattedMessage } from 'react-intl';
+
+import type { IntlMessageID } from 'i18n/messages';
 
 type FormatedMessageProps = {
   id: IntlMessageID;
@@ -11,8 +13,8 @@ type FormatedMessageProps = {
 const FormattedMessage = (props: FormatedMessageProps) => (
   <ReactFormattedMessage
     values={{
-      span: (value: React.ReactNode) => <span>{value}</span>,
-      strong: (value: React.ReactNode) => <strong>{value}</strong>,
+      span: (value: React.ReactNode) => <span key={uuid()}>{value}</span>,
+      strong: (value: React.ReactNode) => <strong key={uuid()}>{value}</strong>,
     }}
     {...props}
   />
