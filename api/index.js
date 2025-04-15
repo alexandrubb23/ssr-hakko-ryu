5c240c482bc45064b0271a4b1b6971b0c1315554
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2025-04-15 14:19:59 +0300

    chore: remove vercel node

--- api/index.js
+++ api/index.js
@@ -1,22 +1,21 @@
 import { readFileSync } from 'fs';
 import { join } from 'path';
-import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-import { render } from '../dist/server/entry-server'; // this is your Vite SSR entry
+const { render } = require('../dist/server/entry-server'); // this is your Vite SSR entry
 
 const indexHtml = readFileSync(
   join(__dirname, '../dist/client/index.html'),
   'utf-8'
 );
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req, res) {
   try {
-    const appHtml = await render(req.url!); // or req.query.url if you prefer
+    const appHtml = await render(req.url); // or req.query.url if you prefer
 
     const html = indexHtml.replace(`<!--app-html-->`, appHtml.html);
 
     res.status(200).setHeader('Content-Type', 'text/html').send(html);
-  } catch (e: any) {
+  } catch (e) {
     console.error(e);
     res.status(500).send('Internal Server Error');
   }
